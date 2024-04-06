d259e870867b9f5d5fc4da9d1df755e047c14157
kwtuku <76758720+kwtuku@users.noreply.github.com>
2024-04-06 18:03:28 +0000

    feat: flyout menu

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,5 +7,8 @@ import { application } from "./application"
 import ElementRemovalController from "./element_removal_controller"
 application.register("element-removal", ElementRemovalController)
 
+import FlyoutMenuController from "./flyout_menu_controller"
+application.register("flyout-menu", FlyoutMenuController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
